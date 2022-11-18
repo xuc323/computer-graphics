a752422a73da29de41f3bcdd5ceea3ce16f5b2e4
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:47:13 -0500

    Project 5: Removed flat shading normals

--- scripts/project5/draw.js
+++ scripts/project5/draw.js
@@ -4,110 +4,110 @@ var pMatrix = mat4.create();
 var normalMatrix = mat3.create();
 var directionalMatrix = vec3.create();
 
-// lighting variables
-var ambientLightColor = [0.5, 0.0, 0.0];
-var directionalLightColor = [1.0, 1.0, 1.0];
-var directionalLightDirection = [1.0, 1.0, 0.0];
-
 function drawScene() {
-    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    // allocate a perspective matrix in pMatrix
-    mat4.perspective(
-        pMatrix,
-        45,
-        gl.viewportWidth / gl.viewportHeight,
-        0.1,
-        100.0
-    );
+  // allocate a perspective matrix in pMatrix
+  mat4.perspective(
+    pMatrix,
+    45,
+    gl.viewportWidth / gl.viewportHeight,
+    0.1,
+    100.0
+  );
 
-    // identify which shader program to use
-    let perFragmentLighting = document.getElementById("shaderCheck").checked;
-    if (perFragmentLighting) {
-        currentProgram = perFragmentProgram;
-    } else {
-        currentProgram = perVertexProgram;
-    }
-    // attach shader program to the openGl context
-    gl.useProgram(currentProgram);
+  // identify which shader program to use
+  let perFragmentLighting = document.getElementById("shaderCheck").checked;
+  if (perFragmentLighting) {
+    currentProgram = perFragmentProgram;
+  } else {
+    currentProgram = perVertexProgram;
+  }
+  // attach shader program to the openGl context
+  gl.useProgram(currentProgram);
 
-    // set ambient light
-    gl.uniform1i(currentProgram.useLightingUniform, true);
-    gl.uniform3fv(currentProgram.ambientColorUniform, ambientLightColor);
+  // set ambient light
+  gl.uniform1i(currentProgram.useLightingUniform, true);
+  gl.uniform3fv(
+    currentProgram.ambientColorUniform,
+    getHTMLObject("ambientColorR", "ambientColorG", "ambientColorB")
+  );
 
-    // set directional light
-    vec3.normalize(directionalMatrix, directionalLightDirection);
-    gl.uniform3fv(
-        currentProgram.lightingDirectionUniform,
-        directionalMatrix
-    );
-    gl.uniform3fv(currentProgram.directionalColorUniform, directionalLightColor);
+  // set directional light
+  vec3.normalize(
+    directionalMatrix,
+    getHTMLObject(
+      "directionalDirectionX",
+      "directionalDirectionY",
+      "directionalDirectionZ"
+    )
+  );
+  gl.uniform3fv(currentProgram.lightingDirectionUniform, directionalMatrix);
+  gl.uniform3fv(
+    currentProgram.directionalColorUniform,
+    getHTMLObject("directionalColorR", "directionalColorG", "directionalColorB")
+  );
 
-    /*************************
-     * START DRAWING SPHERE
-     *************************/
-    // reset the model view matrix
-    mat4.identity(mvMatrix);
-    // set the position of the sphere
-    mat4.translate(mvMatrix, mvMatrix, [0, 0, -10.0]);
-    mat4.multiply(mvMatrix, mvMatrix, mouseRotMatrix);
-    mat4.rotate(mvMatrix, mvMatrix, 90, [1, 0, 0]);
+  /*************************
+   * START DRAWING SPHERE
+   *************************/
+  // reset the model view matrix
+  mat4.identity(mvMatrix);
+  // set the position of the sphere
+  mat4.translate(mvMatrix, mvMatrix, [0, 0, -10.0]);
+  mat4.multiply(mvMatrix, mvMatrix, mouseRotMatrix);
+  mat4.rotate(mvMatrix, mvMatrix, 90, [1, 0, 0]);
 
-    // positions
-    gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer.positionBuffer);
-    gl.vertexAttribPointer(
-        currentProgram.vertexPositionAttribute,
-        sphereBuffer.positionBuffer.itemSize,
-        gl.FLOAT,
-        false,
-        0,
-        0
-    );
+  // positions
+  gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer.positionBuffer);
+  gl.vertexAttribPointer(
+    currentProgram.vertexPositionAttribute,
+    sphereBuffer.positionBuffer.itemSize,
+    gl.FLOAT,
+    false,
+    0,
+    0
+  );
 
-    // normals
-    gl.bindBuffer(gl.ARRAY_BUFFER, perFragmentLighting ? sphereBuffer.normPerFragBuffer : sphereBuffer.normPerVertexBuffer);
-    gl.vertexAttribPointer(
-        currentProgram.vertexNormalAttribute,
-        perFragmentLighting ? sphereBuffer.normPerFragBuffer.itemSize : sphereBuffer.normPerVertexBuffer.itemSize,
-        gl.FLOAT,
-        false,
-        0,
-        0
-    );
+  // normals
+  gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer.normalBuffer);
+  gl.vertexAttribPointer(
+    currentProgram.vertexNormalAttribute,
+    sphereBuffer.normalBuffer.itemSize,
+    gl.FLOAT,
+    false,
+    0,
+    0
+  );
 
-    // texture coordinates
-    gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer.textureCoordBuffer);
-    gl.vertexAttribPointer(
-        currentProgram.textureCoordAttribute,
-        sphereBuffer.textureCoordBuffer.itemSize,
-        gl.FLOAT,
-        false,
-        0,
-        0
-    );
+  // texture coordinates
+  gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer.textureCoordBuffer);
+  gl.vertexAttribPointer(
+    currentProgram.textureCoordAttribute,
+    sphereBuffer.textureCoordBuffer.itemSize,
+    gl.FLOAT,
+    false,
+    0,
+    0
+  );
 
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, sphereBuffer.texture);
-    gl.uniform1i(currentProgram.samplerUniform, 0);
-    // set uniform variable in vertex shader
-    setMatrixUniforms();
-    // draw the triangles
-    gl.drawArrays(gl.TRIANGLES, 0, sphereBuffer.positionBuffer.numItems);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-    /*************************
-     * END DRAWING SPHERE
-     *************************/
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, sphereBuffer.texture);
+  gl.uniform1i(currentProgram.samplerUniform, 0);
+  // set uniform variable in vertex shader
+  setMatrixUniforms();
+  // draw the triangles
+  gl.drawArrays(gl.TRIANGLES, 0, sphereBuffer.positionBuffer.numItems);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+  /*************************
+   * END DRAWING SPHERE
+   *************************/
 }
 
 // Handles the moment by moment re-rendering
 function tick() {
-    requestAnimFrame(tick); // infinite recursive call
-    resize(canvas); // resize the canvas to the window size
-
-    ambientLightColor = getHTMLObject("ambientColorR", "ambientColorG", "ambientColorB");
-    directionalLightColor = getHTMLObject("directionalColorR", "directionalColorG", "directionalColorB");
-    directionalLightDirection = getHTMLObject("directionalDirectionX", "directionalDirectionY", "directionalDirectionZ");
-
-    drawScene(); // draw the scene
+  requestAnimFrame(tick); // infinite recursive call
+  resize(canvas); // resize the canvas to the window size
+  drawScene(); // draw the scene
 }
--- scripts/project5/init.js
+++ scripts/project5/init.js
@@ -2,94 +2,72 @@
 ///// Initialize buffers and textures /////
 ///////////////////////////////////////////
 var sphereBuffer = {
-    positionBuffer: null, // vertex position buffer
-    textureCoordBuffer: null, // vertex texture coordinate buffer
-    normPerVertexBuffer: null, // normal per vertex buffer
-    normPerFragBuffer: null, // vertex normal buffer for per fragment lighting
-    texture: null, // texture buffer
+  positionBuffer: null, // vertex position buffer
+  textureCoordBuffer: null, // vertex texture coordinate buffer
+  normalBuffer: null, // vertex normal buffer
+  texture: null, // texture buffer
 };
 
 // We will generate the geometry with this function
 function initBuffers() {
-    /*************************
-     * START SPHERE BUFFER
-     *************************/
-    // Create a buffer for the sun's vertices.
-    let radius = 1.8,
-        slices = 12,
-        stacks = 6;
-    let { vertices, perVertexNormals, perFragNormals, textureCoords } = createSphere(
-        radius,
-        slices,
-        stacks
-    );
-    // positions
-    sphereBuffer.positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer.positionBuffer);
-    gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array(vertices),
-        gl.STATIC_DRAW
-    );
-    // item size is always 3 (3d vector describing position)
-    sphereBuffer.positionBuffer.itemSize = 3;
-    // numItems is the number of vertices in the triangle (6 * (slices) * stacks)
-    sphereBuffer.positionBuffer.numItems = 6 * slices * stacks;
+  /*************************
+   * START SPHERE BUFFER
+   *************************/
+  // Create a buffer for the sun's vertices.
+  let radius = 1.8,
+    slices = 12,
+    stacks = 6;
+  let { vertices, normals, textureCoords } = createSphere(
+    radius,
+    slices,
+    stacks
+  );
+  // positions
+  sphereBuffer.positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer.positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  // item size is always 3 (3d vector describing position)
+  sphereBuffer.positionBuffer.itemSize = 3;
+  // numItems is the number of vertices in the triangle (6 * (slices) * stacks)
+  sphereBuffer.positionBuffer.numItems = 6 * slices * stacks;
 
-    // texture coordinates
-    sphereBuffer.textureCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer.textureCoordBuffer);
-    gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array(textureCoords),
-        gl.STATIC_DRAW
-    );
-    sphereBuffer.textureCoordBuffer.itemSize = 2;
-    sphereBuffer.textureCoordBuffer.numItems = 6 * slices * stacks;
+  // texture coordinates
+  sphereBuffer.textureCoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer.textureCoordBuffer);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(textureCoords),
+    gl.STATIC_DRAW
+  );
+  sphereBuffer.textureCoordBuffer.itemSize = 2;
+  sphereBuffer.textureCoordBuffer.numItems = 6 * slices * stacks;
 
-    // per vertex normals
-    sphereBuffer.normPerVertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer.normPerVertexBuffer);
-    gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array(perVertexNormals),
-        gl.STATIC_DRAW
-    );
-    sphereBuffer.normPerVertexBuffer.itemSize = 3;
-    sphereBuffer.normPerVertexBuffer.numItems = 6 * slices * stacks;
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-    // per fragment normals
-    sphereBuffer.normPerFragBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer.normPerFragBuffer);
-    gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array(perFragNormals),
-        gl.STATIC_DRAW
-    );
-    sphereBuffer.normPerFragBuffer.itemSize = 3;
-    sphereBuffer.normPerFragBuffer.numItems = 6 * slices * stacks;
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-    /*************************
-     * END SPHERE BUFFER
-     *************************/
+  // normals
+  sphereBuffer.normalBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, sphereBuffer.normalBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+  sphereBuffer.normalBuffer.itemSize = 3;
+  sphereBuffer.normalBuffer.numItems = 6 * slices * stacks;
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  /*************************
+   * END SPHERE BUFFER
+   *************************/
 }
 
 // generate texture
 function initTexture() {
-    // sphere texture
-    sphereBuffer.texture = gl.createTexture();
-    sphereBuffer.texture.image = new Image();
-    sphereBuffer.texture.image.onload = function () {
-        handleLoadedTexture(sphereBuffer.texture);
-    };
-    sphereBuffer.texture.image.src = "../assets/uranus.gif";
+  // sphere texture
+  sphereBuffer.texture = gl.createTexture();
+  sphereBuffer.texture.image = new Image();
+  sphereBuffer.texture.image.onload = function () {
+    handleLoadedTexture(sphereBuffer.texture);
+  };
+  sphereBuffer.texture.image.src = "../assets/uranus.gif";
 }
 
 // Initialize shader programs
 var currentProgram, perVertexProgram, perFragmentProgram;
-
 function initShaders() {
-    perVertexProgram = createProgram("shader-vs", "shader-fs");
-    perFragmentProgram = createProgram("perfrag-shader-vs", "perfrag-shader-fs");
+  perVertexProgram = createProgram("shader-vs", "shader-fs");
+  perFragmentProgram = createProgram("perfrag-shader-vs", "perfrag-shader-fs");
 }
